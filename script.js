59074f3f52e7499de887e773b26fdb1613436571
Caio Beneton <caio.beneton@gmail.com>
2022-08-17 23:26:32 -0300

    feat: Implementado o gerador de cartas

---
+++ script.js
@@ -0,0 +1,10 @@
+let qtdeCartas = 0;
+while (qtdeCartas < 4 || qtdeCartas > 14 || qtdeCartas % 2 !== 0) {
+   qtdeCartas = prompt('Com quantas cartas você quer jogar? \nEscolha um número par entre 4 e 14')
+}
+
+const ul = document.querySelector('ul');
+
+for (let i = 0; i < qtdeCartas; i++) {
+    ul.innerHTML += `<li><div class="carta"><img src="ParrotsAssets/front.png" alt=""></div></li>`
+}
