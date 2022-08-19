79f45f935b32e445752041ea41b2422c9510bd7b
Caio Beneton <caio.beneton@gmail.com>
2022-08-19 06:20:32 -0300

    Feat: movimento e emabaralhar cartas

--- script.js
+++ script.js
@@ -1,10 +1,61 @@
+let cartasViradas = 0;
+let jogadas = 0;
+
 let qtdeCartas = 0;
 while (qtdeCartas < 4 || qtdeCartas > 14 || qtdeCartas % 2 !== 0) {
    qtdeCartas = prompt('Com quantas cartas você quer jogar? \nEscolha um número par entre 4 e 14')
 }
 
+const deck = ['assets/bobrossparrot.gif','assets/explodyparrot.gif','assets/fiestaparrot.gif','assets/metalparrot.gif','assets/revertitparrot.gif','assets/tripletsparrot.gif','assets/unicornparrot.gif'];
+
+deck.sort(comparador);
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+const novoDeck = [];
+
+for (let i = 0; i < qtdeCartas/2; i++) {
+    novoDeck[i] = deck[i];
+}
+
+const deckPronto = [...novoDeck, ...novoDeck];
+deckPronto.sort(comparador);
+
 const ul = document.querySelector('ul');
 
 for (let i = 0; i < qtdeCartas; i++) {
-    ul.innerHTML += `<li><div class="carta"><img src="assets/front.png" alt=""></div></li>`
+    ul.innerHTML += `<li>
+        <div class="carta" onclick="viraCarta(this)">
+            <img class='carta-frente' src="assets/front.png" alt="">
+            <img class='carta-costas' src="${deckPronto[i]}" alt="">
+        </div>
+        </li>`
+}
+function viraCarta(item) {
+    cartasViradas++ // NÃO TA SUBTRAINDO POR ISSO CARTAS NÃO VIRAM DE VOLTA, PRECISA ATUALIZAR
+    
+    const fala = item.querySelector('.carta :nth-child(2)')
+    const falaimg = fala.getAttribute('src')
+    console.log(falaimg)
+
+    if (cartasViradas <= 2) {
+        item.classList.add('virada')
+        setTimeout(desviraCarta, 5000, item)
+    }
+    if (cartasViradas === 2) {
+        jogadas++
+        confereCartas()
+    }
+}
+function desviraCarta(item) {
+    item.classList.remove('virada')
+}
+function confereCartas() {
+    const cartasCheck = document.querySelectorAll('.virada:not(.igual)')
+
+    if (cartasCheck[0].querySelector('.carta :nth-child(2)') === cartasCheck[1].querySelector('.carta :nth-child(2)')){
+
+    }
 }
